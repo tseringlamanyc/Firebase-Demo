92828827a870c3475f7908c93702ab21aa193208
Tsering Lama <tseringlama@pursuit.org>
2020-03-02 10:33:55 -0500

    added tab bar controller to transition from the login screen

--- Firebase-Demo/Controllers/LoginViewController.swift
+++ Firebase-Demo/Controllers/LoginViewController.swift
@@ -9,56 +9,94 @@
 import UIKit
 
 enum AccountState {
-  case existingUser
-  case newUser
+    case existingUser
+    case newUser
 }
 
 class LoginViewController: UIViewController {
-  
-  @IBOutlet weak var errorLabel: UILabel!
-  @IBOutlet weak var containerView: UIView!
-  @IBOutlet weak var emailTextField: UITextField!
-  @IBOutlet weak var passwordTextField: UITextField!
-  @IBOutlet weak var loginButton: UIButton!
-  @IBOutlet weak var accountStateMessageLabel: UILabel!
-  @IBOutlet weak var accountStateButton: UIButton!
-  
-  private var accountState: AccountState = .existingUser
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    clearErrorLabel()
-  }
-  
-  @IBAction func loginButtonPressed(_ sender: UIButton) {
     
-  }
-  
-  private func clearErrorLabel() {
-    errorLabel.text = ""
-  }
-  
-  @IBAction func toggleAccountState(_ sender: UIButton) {
-    // change the account login state
-    accountState = accountState == .existingUser ? .newUser : .existingUser
+    @IBOutlet weak var errorLabel: UILabel!
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var accountStateMessageLabel: UILabel!
+    @IBOutlet weak var accountStateButton: UIButton!
     
-    // animation duration
-    let duration: TimeInterval = 1.0
+    private var accountState: AccountState = .existingUser
     
-    if accountState == .existingUser {
-      UIView.transition(with: containerView, duration: duration, options: [.transitionCrossDissolve], animations: {
-        self.loginButton.setTitle("Login", for: .normal)
-        self.accountStateMessageLabel.text = "Don't have an account ? Click"
-        self.accountStateButton.setTitle("SIGNUP", for: .normal)
-      }, completion: nil)
-    } else {
-      UIView.transition(with: containerView, duration: duration, options: [.transitionCrossDissolve], animations: {
-        self.loginButton.setTitle("Sign Up", for: .normal)
-        self.accountStateMessageLabel.text = "Already have an account ?"
-        self.accountStateButton.setTitle("LOGIN", for: .normal)
-      }, completion: nil)
+    private var authSession = AuthenticationSession()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        clearErrorLabel()
     }
-  }
-
+    
+    @IBAction func loginButtonPressed(_ sender: UIButton) {
+        guard let email = emailTextField.text, !email.isEmpty, let password = passwordTextField.text, !password.isEmpty else {
+            return
+        }
+        continueLogin(email: email, password: password)
+    }
+    
+    private func continueLogin(email: String, password: String) {
+        if accountState == .existingUser {
+            authSession.signExistingUser(email: email, password: password) { [weak self] (result) in
+                switch result {
+                case .failure(let error):
+                    DispatchQueue.main.async {
+                        self?.errorLabel.text = "Error:\(error)"
+                        self?.errorLabel.textColor = .systemRed
+                    }
+                case .success(let authData):
+                    DispatchQueue.main.async {
+                        self?.errorLabel.textColor = .systemGray
+                        self?.errorLabel.text = "Lets fighting love: \(authData.user.email ?? "bawls")"
+                    }
+                }
+            }
+        } else {
+            authSession.createNewUser(email: email, password: password) { [weak self] (result) in
+                switch result {
+                case .failure(let error):
+                    DispatchQueue.main.async {
+                        self?.errorLabel.text = "Error:\(error.localizedDescription)"
+                        self?.errorLabel.textColor = .systemRed
+                    }
+                case .success(let authData):
+                    DispatchQueue.main.async {
+                        self?.errorLabel.textColor = .systemGray
+                        self?.errorLabel.text = "First time \(authData.user.email ?? "")"
+                    }
+                }
+            }
+        }
+    }
+    
+    private func clearErrorLabel() {
+        errorLabel.text = ""
+    }
+    
+    @IBAction func toggleAccountState(_ sender: UIButton) {
+        // change the account login state
+        accountState = accountState == .existingUser ? .newUser : .existingUser
+        
+        // animation duration
+        let duration: TimeInterval = 1.0
+        
+        if accountState == .existingUser {
+            UIView.transition(with: containerView, duration: duration, options: [.transitionCrossDissolve], animations: {
+                self.loginButton.setTitle("Login", for: .normal)
+                self.accountStateMessageLabel.text = "Don't have an account ? Click"
+                self.accountStateButton.setTitle("SIGNUP", for: .normal)
+            }, completion: nil)
+        } else {
+            UIView.transition(with: containerView, duration: duration, options: [.transitionCrossDissolve], animations: {
+                self.loginButton.setTitle("Sign Up", for: .normal)
+                self.accountStateMessageLabel.text = "Already have an account ?"
+                self.accountStateButton.setTitle("LOGIN", for: .normal)
+            }, completion: nil)
+        }
+    }
+    
 }
-
