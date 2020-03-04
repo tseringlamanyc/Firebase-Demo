e3a12c00e72c13221831017020fcadae9af44544
Tsering Lama <tseringlama@pursuit.org>
2020-03-04 10:26:02 -0500

    created a xib file for the table view cell

---
+++ Firebase-Demo/Views/ItemCell.swift
@@ -0,0 +1,22 @@
+//
+//  ItemCell.swift
+//  Firebase-Demo
+//
+//  Created by Tsering Lama on 3/4/20.
+//  Copyright © 2020 Alex Paul. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+}
